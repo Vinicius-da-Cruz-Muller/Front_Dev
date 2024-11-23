1f2413ac30080bb0b52eba38e82cd6da2862a108
Vinicius-da-Cruz-Muller <108363297+Vinicius-da-Cruz-Muller@users.noreply.github.com>
2024-11-23 15:55:17 -0300

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/',//mudar aqui
+  baseURL: 'http://127.0.0.1:5000/',//mudar aqui
 });
 
 export default api;
